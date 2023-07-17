f733d492fe349f280463299c2a3263ba3597cee2
Daya Shankar <dayashankar2001@gmail.com>
2023-07-17 20:11:56 +0530

    Home page modified

--- public/tasks.js
+++ public/tasks.js
@@ -12,32 +12,30 @@ const formatedDate = today.toLocaleDateString("en-US", options);
 
 datePlaceholder.textContent = formatedDate;
 
-// home.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Hide the edit panel initially
+//   var editPanel = document.getElementById("editPanel");
+//   editPanel.style.display = "none";
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Hide the edit panel initially
-  var editPanel = document.getElementById("editPanel");
-  editPanel.style.display = "none";
+//   // Click event handler for items
+//   var items = document.getElementsByClassName("item");
+//   for (var i = 0; i < items.length; i++) {
+//     items[i].addEventListener("click", function () {
+//       // Get the item name
+//       var itemName = this.querySelector("p").textContent;
 
-  // Click event handler for items
-  var items = document.getElementsByClassName("item");
-  for (var i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", function () {
-      // Get the item name
-      var itemName = this.querySelector("p").textContent;
+//       // Set the item name in the edit panel
+//       var editedItemNameInput = editPanel.querySelector(
+//         "input[name='editedItemName']"
+//       );
+//       editedItemNameInput.value = itemName;
 
-      // Set the item name in the edit panel
-      var editedItemNameInput = editPanel.querySelector(
-        "input[name='editedItemName']"
-      );
-      editedItemNameInput.value = itemName;
-
-      // Toggle the visibility of the edit panel
-      if (editPanel.style.display === "none") {
-        editPanel.style.display = "block";
-      } else {
-        editPanel.style.display = "none";
-      }
-    });
-  }
-});
+//       // Toggle the visibility of the edit panel
+//       if (editPanel.style.display === "none") {
+//         editPanel.style.display = "block";
+//       } else {
+//         editPanel.style.display = "none";
+//       }
+//     });
+//   }
+// });
